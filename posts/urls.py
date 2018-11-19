d5469af71080a42a5ff3d7fbaaed9f85c98acb05
changhee <change@hphk.kr>
2018-11-19 02:31:35 +0000

    cbv

--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('cbv/',views.CBView.as_view()),
     path('cbv_index/',views.IndexView.as_view()),
-    path('list/',views.SchoolListView.as_view()),
+    path('list/',views.SchoolListView.as_view(),name='list'),
     path('<int:pk>/',views.SchoolDetailView.as_view()),
+    path('create/',views.SchoolCreateView.as_view(),name='create'),
+    path('update/<int:pk>/',views.SchoolUpdateView.as_view()),
+    path('delete/<int:pk>/',views.SchoolDeleteView.as_view(),name='delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView,ListView,DetailView
+from django.views.generic import (View,TemplateView,
+                                ListView,DetailView,
+                                CreateView,UpdateView,
+                                DeleteView)
 from django.http import HttpResponse
-
+from django.urls import reverse_lazy
 from .models import School
 
 # Create your views here.
@@ -29,4 +32,15 @@ class SchoolListView(ListView):
 class SchoolDetailView(DetailView):
     model = School
     template_name = 'posts/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    model = School
+    fields = ('name','location')
     
+class SchoolUpdateView(UpdateView):
+    model = School
+    fields = ('name',)
+
+class SchoolDeleteView(DeleteView):
+    model = School
+    success_url = reverse_lazy("posts:list")
